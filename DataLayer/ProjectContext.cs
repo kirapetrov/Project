a22769bb1edf6efe57a393fdfc7f206fbd65efb4
kpetrov <kpetrov@ntoire-polus.ru>
2020-01-06 17:58:30 +0300

    context configuration chaged

--- DataLayer/ProjectContext.cs
+++ DataLayer/ProjectContext.cs
@@ -8,10 +8,7 @@ namespace DataLayer
         public DbSet<UserEntity> Users { get; set; }
 
         public DbSet<DeviceEntity> Devices { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseNpgsql("Host=127.0.0.1;Port=5432;Database=Project;Username=kira;Password=kira5533");
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UserEntity>(
